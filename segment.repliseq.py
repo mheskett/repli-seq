638ac43f8b4bdf3e1335196c2a22a9f91190887b
mheskett <mbheskett@gmail.com>
2023-05-02 13:20:54 -0700

    done

--- segment.repliseq.py
+++ segment.repliseq.py
@@ -53,44 +53,6 @@ def add_arms(df):
 	return df
 
 
-chromosomes = ["chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12",
-            "chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX"]
-arms = ["p","q"]
-
-#### for arm level data to skip over centromeres 
-#### hg38              
-cytoband = pd.read_table("cytoBand.ucsc.hg38.txt",sep="\t",
-                        names =["chrom","start","stop","arm","band"])
-arm_dict = get_arms(cytoband)
-
-## hg38
-chromosome_length = {"chr1":248956422,
-"chr2":    242193529,
-"chr3":    198295559,
-"chr4":    190214555,
-"chr5":    181538259,
-"chr6":    170805979,
-"chr7":    159345973,
-"chrX":    156040895,
-"chr8":    145138636,
-"chr9":   138394717,
-"chr11":   135086622,
-"chr10":   133797422,
-"chr12":   133275309,
-"chr13":   114364328,
-"chr14":   107043718,
-"chr15":   101991189,
-"chr16":   90338345,
-"chr17":   83257441,
-"chr18":   80373285,
-"chr20":   64444167,
-"chr19":   58617616,
-"chrY":    57227415,
-"chr22":   50818468,
-"chr21":   46709983}
-
-
-
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="make windows")
 
@@ -104,9 +66,79 @@ if __name__ == "__main__":
 	   metavar="[out file]",
 	   required=True,
 	   help="full path to output results")
+	parser.add_argument("--genome_ref",
+	   type=str,
+	   metavar="[hg19 or hg38]",
+	   required=True,
+	   help="for ref feiles to use")
 
 	arguments = parser.parse_args()
+	######
 
+	if arguments.genome_ref == "hg19":
+		cytoband_path = "cytoBand.hg19.nochr.txt"
+		chromosomes=["1","2","3","4","5","6","7","8","9","10","11","12",
+            "13","14","15","16","17","18","19","20","21","22","X"]
+		chromosome_length = {"1":    249250621,
+						"2":    243199373,
+						"3":    198022430,
+						"4":    191154276,
+						"5":    180915260,
+						"6":    171115067,
+						"7":    159138663,
+						"X":    155270560,
+						"8":    146364022,
+						"9":    141213431,
+						"10":   135534747,
+						"11":   135006516,
+						"12":   133851895,
+						"13":   115169878,
+						"14":   107349540,
+						"15":   102531392,
+						"16":   90354753 ,
+						"17":  81195210 ,
+						"18":   78077248 ,
+						"20":   63025520 ,
+						"Y" :   59373566 ,
+						"19":   59128983 ,
+						"22":   51304566 ,
+						"21":   48129895 }
+
+	elif arguments.genome_ref == "hg38":
+		cytoband_path = "cytoBand.ucsc.hg38.txt"
+		chromosomes = ["chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12",
+            "chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX"]
+		## hg38
+		chromosome_length = {"chr1":248956422,
+		"chr2":    242193529,
+		"chr3":    198295559,
+		"chr4":    190214555,
+		"chr5":    181538259,
+		"chr6":    170805979,
+		"chr7":    159345973,
+		"chrX":    156040895,
+		"chr8":    145138636,
+		"chr9":   138394717,
+		"chr11":   135086622,
+		"chr10":   133797422,
+		"chr12":   133275309,
+		"chr13":   114364328,
+		"chr14":   107043718,
+		"chr15":   101991189,
+		"chr16":   90338345,
+		"chr17":   83257441,
+		"chr18":   80373285,
+		"chr20":   64444167,
+		"chr19":   58617616,
+		"chrY":    57227415,
+		"chr22":   50818468,
+		"chr21":   46709983}
+
+	arms = ["p","q"]
+
+	cytoband = pd.read_table(cytoband_path,sep="\t",
+	                        names =["chrom","start","stop","arm","band"])
+	arm_dict = get_arms(cytoband)
 
 	#######
 	df_repli = pd.read_csv(arguments.repli_bed,
@@ -117,7 +149,6 @@ if __name__ == "__main__":
 
 	#######
 
-	# df_regions = pd.DataFrame(columns=["chrom","start","stop","arm","peak_prominence","peak_width","peak_height","peak_valley"])
 
 	regions=[]
 	for chrom in chromosomes:
@@ -139,7 +170,7 @@ if __name__ == "__main__":
 							tmp.loc[properties["right_ips"][i].round(),:]["start"], 
 							arm, 
 							properties["prominences"][i], 
-							properties["widths"][i],  
+							tmp.loc[properties["right_ips"][i].round(),:]["start"] - tmp.loc[properties["left_ips"][i].round(),:]["start"],  
 							properties["peak_heights"][i], 
 							"peak" ]   ]
 
@@ -149,7 +180,7 @@ if __name__ == "__main__":
 							tmp.loc[valley_properties["right_ips"][i].round(),:]["start"], 
 							arm, 
 							valley_properties["prominences"][i], 
-							valley_properties["widths"][i],  
+							tmp.loc[valley_properties["right_ips"][i].round(),:]["start"] - tmp.loc[valley_properties["left_ips"][i].round(),:]["start"],  
 							-valley_properties["peak_heights"][i], 
 							"valley" ]   ]
 
@@ -183,12 +214,31 @@ if __name__ == "__main__":
 			                        height=20, fill=True, alpha=0.5,facecolor="green")
 			    ax.add_patch(rect)
 			plt.suptitle(chrom+"_"+arm)
+			plt.margins(0,0)
+			plt.savefig(arguments.repli_bed.rstrip(".bed")+"."+chrom+arm+".pdf")
 	result = pd.DataFrame(regions,columns=["chrom","start","stop","arm","peak_prominence","peak_width","peak_height","peak_valley"])
 
-
-	
-	## limitation find_peaks works within sequence space range(len(repli-seq)),
-	## not actually with the genomic coordinate
+	result.to_csv(arguments.repli_bed.rstrip(".bed")+"_peaks.bed",header=None,index=None,sep="\t")
+	result.to_csv(arguments.repli_bed.rstrip(".bed")+"_peaks.txt",header=True,index=None,sep="\t")
 
 
+	plt.figure()
+	plt.hist(result["peak_prominence"],bins=30)
+	# plt.xticks(list(range(0,16)))
+	plt.suptitle("peak prominence")
+	plt.savefig(arguments.repli_bed.rstrip(".bed")+"peak_prominence.pdf")
 
+	plt.figure()
+	plt.hist(result["peak_width"],bins=30)
+	# plt.xticks(list(range(0,16)))
+	plt.suptitle("peak width")
+	plt.savefig(arguments.repli_bed.rstrip(".bed")+"peak_width.pdf")
+
+	plt.figure()
+	plt.hist(result["peak_height"],bins=30)
+	# plt.xticks(list(range(0,16)))
+	plt.suptitle("peak height")
+	plt.savefig(arguments.repli_bed.rstrip(".bed")+"peak_height.pdf")
+
+
+	print(result)
